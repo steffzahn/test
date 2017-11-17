341762c0f7b255f6d6519af672e508b0cafee42f
Steffen Zahn <steffzahn@gmail.com>
2017-11-17 13:00:09 +0000

    update go experiment

--- go/hello.go
+++ go/hello.go
@@ -5,6 +5,7 @@ import "unsafe"
 import "reflect"
 import "os"
 import "math/cmplx"
+import "strconv"
 
 func sum(a []int) int {
 	z := 0
@@ -13,6 +14,23 @@ func sum(a []int) int {
 	}
 	a = append(a, 9, 8)
 	return z
+}
+
+type Stringer interface {
+	String() string
+}
+
+func toString(any interface{}) string {
+	if v, ok := any.(Stringer); ok {
+		return v.String()
+	}
+	switch v := any.(type) {
+	case int:
+		return strconv.Itoa(v)
+	case float64:
+		return strconv.FormatFloat(v, 'g', -1, 32)
+	}
+	return "???"
 }
 
 func main() {
@@ -32,4 +50,14 @@ func main() {
 	arr := []int{3, 4, 6, 7}
 	fmt.Println(sum(arr[:2]))
 	fmt.Println(arr)
+	fmt.Println(toString(33))
+	xi := interface{}(struct{ s string }{"WTF"})
+	switch xi.(type) {
+	case int:
+		fmt.Println("int")
+	case float64:
+		fmt.Println("float64")
+	default:
+		fmt.Println("???")
+	}
 }
