ff6d7deef9378d4277ee182e994ccd84e68d66b9
Steffen Zahn <zahn@snafu.de>
2018-01-26 14:20:52 +0100

    Spring test program

---
+++ spring/src/main/java/sz/spring/SpringTest.java
@@ -0,0 +1,21 @@
+package sz.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class SpringTest {
+    @RequestMapping("/")
+    String home()
+    {
+        return "Hello world!";
+    }
+
+    public static void main( String[] args) throws Exception
+    {
+        SpringApplication.run(SpringTest.class, args);
+    }
+}
